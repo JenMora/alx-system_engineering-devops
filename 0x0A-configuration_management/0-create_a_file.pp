0f8460dcd657eeb59347a5ea0fc061e0ee037a40
JenMora <jeniffermourah73@gmail.com>
2023-10-27 08:39:52 -0700

    Thisis a script that creates a file in Puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # create a file in /tmp
 file {'school':
-  path => '/tmp/school':
-  mode    => '0744',
+  path    => '/tmp/school',
   owner   => 'www-data',
   group   => 'www-data',
-  content => "I love Puppet\n", # Use double quotes to avoid issues with escape
+  mode    => '0744',
+  content => 'I love Puppet',
 }
