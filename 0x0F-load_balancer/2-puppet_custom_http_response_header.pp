4f4f3458229230541e7064d6ec6c6270059f3530
JenMora <jeniffermourah73@gmail.com>
2023-11-22 22:12:39 -0800

    I cleared the load alancer backlog

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,6 +1,7 @@
-# Automate the task of creating a custom HTTP header response,with Puppet
+# Automate the task of creating a custom HTTP header response, but with Puppet
 exec { 'command':
   command  => 'apt-get update; apt-get install -y nginx;
-  sed -i "/server_name _/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
+  sed -i "51i add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
   service nginx restart',
   provider => shell,
+}
