0b5c763779b81bff49dcb8861b9ec6ff5e5b9719
JenMora <jeniffermourah73@gmail.com>
2023-11-27 12:27:34 -0800

    extend your Python script to export data in the CSV format

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,40 +1,45 @@
 #!/usr/bin/python3
 """
-This is a script that Uses REST API for a given employee ID to return
+Use REST API for a given employee ID to return
 information about his/her TODO list progress
+Export data to CSV format
 """
 
 
+import csv
 import requests
 import sys
-import cv
 
 
 def todo_list(employee_id):
     """
-    This method returns information about an employee's TODO list
+    Returns information about an employee's TODO list
     """
     url_api = f'https://jsonplaceholder.typicode.com/'\
               f'todos?userId={employee_id}'
     try:
+        # make GET request to todo list API endpoint
         response = requests.get(url_api)
         response.raise_for_status()
         todo = response.json()
+        # make separate GET request to user endpoint for user info
         user_response = requests.get(f'https://jsonplaceholder.typicode.com/'
                                      f'users/{employee_id}')
         user_response.raise_for_status()
         user_info = user_response.json()
-        employee_name = user_info['name']
+        user_id = user_info['id']
+        username = user_info['name']
         completed_tasks = []
         for task in todo:
             if task['completed']:
                 completed_tasks.append(task)
-        total_tasks = len(todo)
-        completed_task_num = len(completed_tasks)
-        print(f"Employee {employee_name} is done with tasks"
-              f"({str(completed_task_num)}/{str(total_tasks)}):")
-        for task in completed_tasks:
-            print(f"\t {task['title']}")
+        return {
+                'user_id': user_id,
+                'username': username,
+                'total_tasks': len(todo),
+                'tasks': todo,
+                'user_info': user_info
+                }
     except requests.exceptions.RequestException as e:
         print(e)
         sys.exit(1)
@@ -42,27 +47,8 @@ def todo_list(employee_id):
 
 def export_csv(employee_id, total_tasks, tasks, user_info):
     """
-    Export data in CSV format.
-
-    Parameters:
-        employee_id (str): The ID of the employee for whom tasks
-        are being exported.
-        total_tasks (int): The total number of tasks.
-        tasks (list): A list of dictionaries representing tasks,each containing
-        'completed' and 'title' keys.
-        user_info (dict): A dictionary containing user information,
-        including the 'username'.
-
-    Returns:
-        None
-
-    Generates a CSV file named '{employee_id}.csv' with the following columns:
-        - USER_ID: The employee ID.
-        - USERNAME: The username associated with the employee.
-        - TASK_COMPLETED_STATUS: The completion status of each task.
-        - TASK_TITLE: The title of each task.
+    Export data in CSV format
     """
-
     file = f"{employee_id}.csv"
     with open(file, 'w', newline='') as csvfile:
         fieldnames = ['USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS',
@@ -83,7 +69,9 @@ if __name__ == "__main__":
         sys.exit(1)
     try:
         employee_id = int(sys.argv[1])
-        todo_list(employee_id)
+        todo_info = todo_list(employee_id)
+        export_csv(employee_id, todo_info['total_tasks'],
+                   todo_info['tasks'], todo_info['user_info'])
     except ValueError as e:
         print(e)
         sys.exit(1)
